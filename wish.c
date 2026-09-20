eea55d3c480fa504b6cf7080ce39621342d3818a
Mariia Komar <mashroomnet0501@gmail.com>
2026-09-20 14:59:55 +0000

    command to parse arguments

--- wish.c
+++ wish.c
@@ -16,13 +16,36 @@ int main() {
         // Remove newline character
         line[strcspn(line, "\n")] = '\0';
 
+        // Skip empty input
+        if (strlen(line) == 0) {
+            continue;
+        }
+
         // Built-in exit command
         if (strcmp(line, "exit") == 0) {
             free(line);
             exit(0);
         }
 
-        printf("You entered: %s\n", line);
+        // Parse command into arguments
+        char *args[100];
+        int argc = 0;
+
+        char *token = strtok(line, " ");
+
+        while (token != NULL && argc < 99) {
+            args[argc] = token;
+            argc++;
+
+            token = strtok(NULL, " ");
+        }
+
+        args[argc] = NULL;
+
+        // debug output
+        for (int i = 0; i < argc; i++) {
+            printf("args[%d] = %s\n", i, args[i]);
+        }
     }
 
     free(line);
