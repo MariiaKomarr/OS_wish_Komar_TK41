939fd96897af8b361bac08704c9b855bfbd3b876
Mariia Komar <mashroomnet0501@gmail.com>
2026-09-20 14:25:55 +0000

    add input

---
+++ wish.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *line = NULL;
+    size_t len = 0;
+
+    while (1) {
+        printf("wish> ");
+
+        if (getline(&line, &len, stdin) == -1) {
+            break;
+        }
+
+        printf("You entered: %s", line);
+    }
+
+    free(line);
+
+    return 0;
+}
